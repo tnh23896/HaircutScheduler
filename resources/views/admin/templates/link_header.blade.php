4d623714fc3a361dced40b3814f48f250ead2186
Thanh Tung <tunght2302@gmail.com>
2023-10-08 17:41:24 +0700

    helper-ajax

--- resources/views/admin/templates/link_header.blade.php
+++ resources/views/admin/templates/link_header.blade.php
@@ -2,5 +2,10 @@
     integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 <script src="https://kit.fontawesome.com/86807bc2b2.js" crossorigin="anonymous"></script>
+<meta name="csrf-token" content="{{ csrf_token() }}" />
+<script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM="
+crossorigin="anonymous"></script>
+<script src="{{asset('dist/js/helper-ajax.js')}}"></script>
+
 
 
