58acff0b11c1ce52dfcdfc15cde31f1c02c1e9d3
thanh <hoang212185@gmail.com>
2023-10-15 00:43:46 +0700

    Clean code for readability

--- app/Http/Controllers/Admin/EmployeeManagement/EmployeeController.php
+++ app/Http/Controllers/Admin/EmployeeManagement/EmployeeController.php
@@ -32,24 +32,27 @@ class EmployeeController extends Controller
         return view('admin.employee.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreRequest $request)
-    {
-        try {
-            $params = $request->all();
-            if($request->hasFile('avatar') && $request->file('avatar')->isValid()) {
-                $file = $request->file('avatar');
-               $name= upload_file('admin/avatar',$file);
-                $params['avatar'] = $name;
-            }
-            $employee = Admin::create($params);
-            return to_route('admin.employee.index')->with('success', 'Created successfully');
-        } catch (Exception $e) {
-            return redirect()->back()->with('error', 'Error: ' . $e->getMessage());
-        }   
-    }
+/**
+ * Store a newly created resource in storage.
+ */
+public function store(StoreRequest $request)
+{
+    try {
+        $requestData = $request->all();
+        
+        if ($request->hasFile('avatar') && $request->file('avatar')->isValid()) {
+            $file = $request->file('avatar');
+            $avatarName = upload_file('admin/avatar', $file);
+            $requestData['avatar'] = $avatarName;
+        }
+        
+        $employee = Admin::create($requestData);
+        
+        return redirect()->route('admin.employee.index')->with('success', 'Created successfully');
+    } catch (Exception $e) {
+        return redirect()->back()->with('error', 'Error: ' . $e->getMessage());
+    }   
+}
 
     /**
      * Display the specified resource.
@@ -59,57 +62,57 @@ class EmployeeController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        try {
-            $employee = Admin::findOrFail($id);
-            return view('admin.employee.edit', compact('employee'));
-        } catch (\Throwable $th) {
-            if($th instanceof ModelNotFoundException) {
-                return response(['message' => 'Not found employee'], 404);
-            }
-            return response(['message' => $th->getMessage()], 500);
-        }
+/**
+ * Show the form for editing the specified resource.
+ */
+public function edit(string $id)
+{
+    try {
+        $employee = Admin::findOrFail($id);
+        return view('admin.employee.edit', compact('employee'));
+    } catch (ModelNotFoundException $exception) {
+        return response(['message' => 'Not found employee'], 404);
+    } catch (\Throwable $exception) {
+        return response(['message' => $exception->getMessage()], 500);
     }
+}
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateRequest $request, string $id)
-    {
-        try {
-            $employee = Admin::findOrFail($id);
-            $params = $request->all();
-            if($request->hasFile('avatar') && $request->file('avatar')->isValid()) {
-                $file = $request->file('avatar');
-               $name= upload_file('admin/avatar',$file);
-               $params['avatar'] = $name;
-               delete_file($employee->avatar);
-            }
-            $employee->update($params);
-            return to_route('admin.employee.index')->with('success', 'Updated successfully');
-        } catch (Exception $e) {
-            if($e instanceof ModelNotFoundException) {
-                return response(['message' => 'Not found employee'], 404);
-            }
-            return response(['message' => $e->getMessage()], 500);
-        }   
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        try {
-            $employee = Admin::findOrFail($id);
-            $employee->delete();
-            return response()->json(['status' => 'success', 'message' => 'Deleted successfully']);
-        } catch (\Throwable $th) {
-            return response()->json(['status' => 'error', 'message' => $th->getMessage()]);
+/**
+ * Update the specified resource in storage.
+ */
+public function update(UpdateRequest $request, string $id)
+{
+    try {
+        $employee = Admin::findOrFail($id);
+        $params = $request->all();
+        
+        if ($request->hasFile('avatar') && $request->file('avatar')->isValid()) {
+            $file = $request->file('avatar');
+            $avatarName = upload_file('admin/avatar', $file);
+            $params['avatar'] = $avatarName;
+            delete_file($employee->avatar);
         }
+        
+        $employee->update($params);
+        return redirect()->route('admin.employee.index')->with('success', 'Updated successfully');
+    } catch (ModelNotFoundException $e) {
+        return response(['message' => 'Not found employee'], 404);
+    } catch (Exception $e) {
+        return response(['message' => $e->getMessage()], 500);
+    }   
+}
+
+/**
+ * Remove the specified resource from storage.
+ */
+public function destroy(string $id)
+{
+    try {
+        $admin = Admin::findOrFail($id);
+        $admin->delete();
+        return response()->json(['status' => 'success', 'message' => 'Deleted successfully']);
+    } catch (\Throwable $exception) {
+        return response()->json(['status' => 'error', 'message' => $exception->getMessage()]);
     }
+}
 }
