99f4ccfde755dc82efe32d62dcb6a931b6d7c5ae
Thachnv278 <137411320+Thachnv278@users.noreply.github.com>
2023-10-14 14:56:28 +0700

    remove line space

--- routes/admin/web.php
+++ routes/admin/web.php
@@ -1,6 +1,5 @@
 <?php
 
-
 use App\Http\Controllers\Admin\TimeManagement\TimeController;
 use App\Http\Controllers\Admin\UserManagement\UserController;
 use App\Http\Controllers\Admin\ScheduleManagement\ScheduleController;
