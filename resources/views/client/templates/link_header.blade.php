2e5f3a6f335756a6be71f43b2e5f63be2cc24162
Duonglor <quyduong1201@gmail.com>
2023-10-17 22:16:05 +0700

    Add_layout

--- resources/views/client/templates/link_header.blade.php
+++ resources/views/client/templates/link_header.blade.php
@@ -1,13 +1,11 @@
-<meta name="csrf-token" content="{{ csrf_token() }}" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
-    integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
-    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="{{asset('js/helper-ajax.js')}}"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-<link href="{{ asset('client/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" id="bootstrap">
-<link href="{{ asset('client/css/mdb.min.csss') }}" rel="stylesheet" type="text/css" id="mdb">
-<link href="{{ asset('client/css/plugins.css') }}" rel="stylesheet" type="text/css">
-<link href="{{ asset('client/css/style.css') }}" rel="stylesheet" type="text/css">
-<link href="{{ asset('client/css/coloring.css') }}" rel="stylesheet" type="text/css">
-<!-- color scheme -->
-<link id="colors" href="{{ asset('client/css/colors/scheme-01.css') }}" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="{{ asset('client/assets/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('client/assets/css/all.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('client/assets/css/slick.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('client/assets/css/slick-theme.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('client/assets/css/jquery.fancybox.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('client/assets/css/animate.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('client/assets/css/owl.carousel.min.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('client/assets/css/aos.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('client/assets/css/color.css') }}">
+<link rel="stylesheet" href="{{ asset('client/assets/css/style.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('client/assets/css/responsive.css') }}">
