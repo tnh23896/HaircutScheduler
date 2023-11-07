561cea9dcf20111be1b697d4da82d881c702ce57
Thanh Tung <tunght2302@gmail.com>
2023-11-07 10:39:01 +0700

    add pagination to client-templates

--- resources/views/client/bill_history/list_bill.blade.php
+++ resources/views/client/bill_history/list_bill.blade.php
@@ -4,20 +4,15 @@
             <div class="order-info">
                 <span class="red-clr font-weight-bold mb-2">ID hóa đơn: #{{ $bill->id }}</span>
                 <span class="price font-weight-bold mb-2">Tổng tiền:
-                                {{ number_format($bill->total_price) }}</span>
+                    {{ number_format($bill->total_price) }}</span>
                 <div class="mt-2 mb-2">
                     <span class="price font-weight-bold">Lịch đặt: {{ $bill->time }} {{ $bill->day }}</span>
                 </div>
                 <span class="price font-weight-bold">Thời gian tạo: {{ $bill->created_at }}</span>
-                <a class="theme-btn-2" data-toggle="modal" data-target=".modal{{ $bill->id }}"
-                   href="" title="" itemprop="url" style="width: 150px;
-                                                       height: 40px;
-                                                       color: white;
-                                                       border: none;
-                                                       padding: 0px;
-                                                       text-align: center;
-                                                       font-size: 13px;
-                                                       border-radius: 3px">Chi tiết hoá đơn</a>
+                <a class="theme-btn-2" data-toggle="modal" data-target=".modal{{ $bill->id }}" href=""
+                    title="" itemprop="url"
+                    style="width: 150px;height: 40px;color: white;border: none;padding: 0px;text-align: center;font-size: 13px;border-radius: 3px">
+                Chi tiết hoá đơn</a>
             </div>
         </div>
     @endforeach
--- resources/views/client/blog/blog.blade.php
+++ resources/views/client/blog/blog.blade.php
@@ -38,52 +38,5 @@
     <section class="py-120 mt-5">
         {{ $listBlogs->links('custom.pagination') }}
     </section>
-
-    <script type="text/javascript">
-        $(window).on('hashchange', function () {
-            if (window.location.hash) {
-                var page = window.location.hash.replace('#', '');
-                if (page == Number.NaN || page <= 0) {
-                    return false;
-                } else {
-                    getData(page);
-                }
-            }
-        });
-        $(document).ready(function () {
-            $(document).on('click', '.pagination a', function (event) {
-                $('li').removeClass('active');
-                $(this).parent('li').addClass('active');
-                event.preventDefault();
-
-                var myurl = $(this).attr('href');
-                var page = $(this).attr('href').split('page=')[1];
-
-                // Kiểm tra giá trị `page` trước khi cập nhật URL
-                if (page === undefined || page === null) {
-                    page = 1; // Giá trị mặc định khi `page` không xác định
-                }
-
-                // Sử dụng History API để thay đổi URL mà không tải lại trang
-                history.replaceState(null, null, '?page=' + page);
-
-                getData(page);
-            });
-        });
-
-        function getData(page) {
-            $.ajax({
-                url: '?page=' + page,
-                type: "get",
-                datatype: "html",
-            })
-                .done(function (data) {
-                    $("#data-wrapper").empty().html("<div class ='container'>" + data + "</div>");
-                    // location.hash = page;
-                })
-                .fail(function (jqXHR, ajaxOptions, thrownError) {
-                    alert('No response from server');
-                });
-        }
-    </script>
+    @include('client.templates.pagination')
 @endsection
