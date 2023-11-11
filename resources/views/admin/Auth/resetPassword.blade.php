232686c4fa62537a7f6b87ca0601ef508a615da1
CaoXuanThang <88951817+CaoXuanThang@users.noreply.github.com>
2023-11-11 21:07:23 +0700

    Merge branch 'develop' of https://github.com/tnh23896/HaircutScheduler into feature/thangcx/login_user

--- resources/views/admin/Auth/resetPassword.blade.php
+++ resources/views/admin/Auth/resetPassword.blade.php
@@ -1,42 +1,44 @@
 <div id="modal" class="modal" tabindex="-1" aria-hidden="true">
     <div class="modal-dialog modal-lg modal-content">
 
-        <div class="content">
-            <div class="grid grid-cols-12 gap-6">
-                <div class="col-span-12 lg:col-span-12 2xl:col-span-9">
-                    <!-- BEGIN: Change Password -->
-                    <div class="intro-y box lg:mt-5">
-                        <div class="flex items-center p-5 border-b border-slate-200/60 dark:border-darkmode-400">
-                            <h2 class="font-medium text-base mr-auto">
-                                Change Password
-                            </h2>
-                        </div>
-                        <div class="p-5">
-                            <form action="{{route('admin.auth.ResetPasswordPost')}}" method="post">
-                                @csrf
-                            <div>
-                                <label for="change-password-form-1" class="form-label">Old Password</label>
-                                <input id="change-password-form-1" type="password" name="current_password" class="form-control" placeholder="Input text" required>
-                            </div>
-                            <div class="mt-3">
-                                <label for="change-password-form-2" class="form-label">New Password</label>
-                                <input id="change-password-form-2" type="password" name="new_password" class="form-control" placeholder="Input text" required>
-                            </div>
-                            <div class="mt-3">
-                                <label for="change-password-form-3" class="form-label">Confirm New Password</label>
-                                <input id="change-password-form-3" type="password" class="form-control" name="new_password_confirmation" placeholder="Input text" required>
-                            </div>
-                            <button type="submit" class="btn btn-primary mt-4">Change Password</button>
-                        </form>
-                        </div>
-                    </div>
-                    <!-- END: Change Password -->
+        <div class="content" style="max-height: 300px !important">
+            <!-- BEGIN: Top Bar -->
+
+            <!-- END: Top Bar -->
+            
+
+
+            <div class="p-5" >
+                <div class="intro-y flex flex-col sm:flex-row items-center mt-8">
+                    <h2 class="text-lg font-medium mr-auto">
+                        Đổi mật khẩu
+                    </h2>
+    
                 </div>
+                <form action="{{ route('admin.auth.ResetPasswordPost') }}" method="post">
+                    @csrf
+                    <div>
+                        <label for="change-password-form-1" class="form-label">Mật khẩu cũ</label>
+                        <input id="change-password-form-1" type="password" name="current_password" class="form-control"
+                            placeholder="Input text" required>
+                    </div>
+                    <div class="mt-3">
+                        <label for="change-password-form-2" class="form-label">Mật khẩu mới</label>
+                        <input id="change-password-form-2" type="password" name="new_password" class="form-control"
+                            placeholder="Input text" required>
+                    </div>
+                    <div class="mt-3">
+                        <label for="change-password-form-3" class="form-label">Nhập lại mật khẩu mới</label>
+                        <input id="change-password-form-3" type="password" class="form-control"
+                            name="new_password_confirmation" placeholder="Input text" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary mt-4">Sửa mật khẩu</button>
+                </form>
             </div>
+
 
 
         </div>
 
     </div>
 </div>
-
