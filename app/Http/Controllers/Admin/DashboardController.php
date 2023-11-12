be97ee26e049420a4561946e7e156d38c75aa6e7
Thanh Tung <tunght2302@gmail.com>
2023-11-12 21:38:52 +0700

    delete reuquest index DashboardController

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -8,9 +8,9 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-	public function index(Request $request)
+	public function index()
 	{
-		$data = $this->baseScheduleSetbyTime($request);
+		$data = $this->baseScheduleSetbyTime();
 		return view('admin.dashboard', compact('data'));
 	}
 
