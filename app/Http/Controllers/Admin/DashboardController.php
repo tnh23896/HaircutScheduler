713a58087e978eed5cb71e0cd20aa09f595c38d4
Duonglor <quyduong1201@gmail.com>
2023-11-15 22:04:13 +0700

    fix config

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -24,86 +24,90 @@ class DashboardController extends Controller
 		$totalRevenue = $this->calculateBillRevenue();
 		$lastMonthrevenue = $this->revenue();
 		$revenue = $this->currentMonthRevenue();
-	
-		
-		return view('admin.dashboard', compact('data', 'totalRevenue', 'topBooker', 'topservice', 'revenue', 'lastMonthrevenue' ,'topEmployeesData','getbill','getadmin','getservice','getuser'));
+
+
+		return view('admin.dashboard', compact('data', 'totalRevenue', 'topBooker', 'topservice', 'revenue', 'lastMonthrevenue', 'topEmployeesData', 'getbill', 'getadmin', 'getservice', 'getuser'));
 	}
-	public function getbill(){
+	public function getbill()
+	{
 		$getbill = DB::table('bills')->count();
 		return $getbill;
 	}
-	public function getadmin(){
+	public function getadmin()
+	{
 		$getadmin = DB::table('admins')->count();
 		return $getadmin;
 	}
-	public function getservice(){
+	public function getservice()
+	{
 		$getservice = DB::table('services')->count();
 		return $getservice;
 	}
-	public function getuser(){
+	public function getuser()
+	{
 		$getuser = DB::table('users')->count();
 		return $getuser;
 	}
 
 	private function revenue()
-{
-    // Lấy ngày hiện tại
-    $today = Carbon::now();
+	{
+		// Lấy ngày hiện tại
+		$today = Carbon::now();
 
-    // Lấy ngày đầu tiên của tháng trước
-    $firstDayOfLastMonth = $today->subMonth()->firstOfMonth();
+		// Lấy ngày đầu tiên của tháng trước
+		$firstDayOfLastMonth = $today->subMonth()->firstOfMonth();
 
-    // Lấy tháng và năm của tháng trước
-    $lastMonth = $firstDayOfLastMonth->format('n');
-    $currentYear = $firstDayOfLastMonth->format('Y');
+		// Lấy tháng và năm của tháng trước
+		$lastMonth = $firstDayOfLastMonth->format('n');
+		$currentYear = $firstDayOfLastMonth->format('Y');
 
-    $query = Bill::selectRaw('MONTH(day) as month, COUNT(*) as totalBills')
-        ->selectRaw('SUM(CASE WHEN promo_id IS NOT NULL THEN total_price - promotions.discount ELSE total_price END) as totalRevenues')
-        ->join('promotions', 'bills.promo_id', '=', 'promotions.id')
-        ->when($lastMonth, function ($query) use ($lastMonth) {
-            return $query->whereMonth('day', $lastMonth);
-        })
-        ->when($currentYear, function ($query) use ($currentYear) {
-            return $query->whereYear('day', $currentYear);
-        })
-        ->groupBy('month')
-        ->orderBy('month')
-        ->get();
+		$query = Bill::selectRaw('MONTH(day) as month, COUNT(*) as totalBills')
+			->selectRaw('SUM(CASE WHEN promo_id IS NULL THEN total_price ELSE total_price - promotions.discount END) as totalRevenues')
+			->leftJoin('promotions', 'bills.promo_id', '=', 'promotions.id')
+			->when($lastMonth, function ($query) use ($lastMonth) {
+				return $query->whereMonth('day', $lastMonth);
+			})
+			->when($currentYear, function ($query) use ($currentYear) {
+				return $query->whereYear('day', $currentYear);
+			})
+			->groupBy('month')
+			->orderBy('month')
+			->get();
 
-    $data = $query->isEmpty() ? 0 : $query[0]->totalRevenues;
+		$data = $query->isEmpty() ? 0 : $query[0]->totalRevenues;
 
-    return $data;
-}
+		return $data;
+	}
 
-private function currentMonthRevenue()
-{
-    // Lấy ngày hiện tại
-    $today = Carbon::now();
+	private function currentMonthRevenue()
+	{
+		// Lấy ngày hiện tại
+		$today = Carbon::now();
 
-    // Lấy ngày đầu tiên của tháng hiện tại
-    $firstDayOfCurrentMonth = $today->firstOfMonth();
+		// Lấy ngày đầu tiên của tháng hiện tại
+		$firstDayOfCurrentMonth = $today->firstOfMonth();
 
-    // Lấy tháng và năm của tháng hiện tại
-    $currentMonth = $firstDayOfCurrentMonth->format('n');
-    $currentYear = $firstDayOfCurrentMonth->format('Y');
+		// Lấy tháng và năm của tháng hiện tại
+		$currentMonth = $firstDayOfCurrentMonth->format('n');
+		$currentYear = $firstDayOfCurrentMonth->format('Y');
 
-    $query = Bill::selectRaw('MONTH(day) as month, COUNT(*) as totalBills')
-        ->selectRaw('SUM(CASE WHEN promo_id IS NOT NULL THEN total_price - promotions.discount ELSE total_price END) as totalRevenues')
-        ->join('promotions', 'bills.promo_id', '=', 'promotions.id')
-        ->when($currentMonth, function ($query) use ($currentMonth) {
-            return $query->whereMonth('day', $currentMonth);
-        })
-        ->when($currentYear, function ($query) use ($currentYear) {
-            return $query->whereYear('day', $currentYear);
-        })
-        ->groupBy('month')
-        ->orderBy('month')
-        ->get();
+		$query = Bill::selectRaw('MONTH(day) as month, COUNT(*) as totalBills')
+			->selectRaw('SUM(CASE WHEN promo_id IS NULL THEN total_price ELSE total_price - promotions.discount END) as totalRevenues')
+			->leftJoin('promotions', 'bills.promo_id', '=', 'promotions.id')
+			->when($currentMonth, function ($query) use ($currentMonth) {
+				return $query->whereMonth('day', $currentMonth);
+			})
+			->when($currentYear, function ($query) use ($currentYear) {
+				return $query->whereYear('day', $currentYear);
+			})
+			->groupBy('month')
+			->orderBy('month')
+			->get();
 
-    $data = $query->isEmpty() ? 0 : $query[0]->totalRevenues;
+		$data = $query->isEmpty() ? 0 : $query[0]->totalRevenues;
 
-    return $data;
-}
+		return $data;
+	}
 	public function revenueSetbyTime(Request $request)
 	{
 		$totalRevenue = $this->baRevenueSetbyTime($request);
@@ -189,12 +193,13 @@ private function currentMonthRevenue()
 	private function calculateBillRevenue()
 	{
 		$query = Bill::selectRaw('MONTH(day) as month, COUNT(*) as totalBills')
-			->selectRaw('SUM(CASE WHEN promo_id IS NOT NULL THEN total_price - promotions.discount ELSE total_price END) as totalRevenues')
-			->join('promotions', 'bills.promo_id', '=', 'promotions.id')
+			->selectRaw('SUM(CASE WHEN promo_id IS NULL THEN total_price ELSE total_price - promotions.discount END) as totalRevenues')
+			->leftJoin('promotions', 'bills.promo_id', '=', 'promotions.id')
 			->groupBy('month')
 			->orderBy('month', 'asc');
 
 		$filteredData = $query->get();
+
 
 		$totalRevenue = [];
 		foreach ($filteredData as $item) {
