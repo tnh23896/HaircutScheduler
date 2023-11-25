eec2f448ad403a1bdb1f17231f7334341f79b84f
tinhnv23123 <nguyenvantinh2310.03@gmial.com>
2023-11-25 12:43:28 +0700

    change name request

--- app/Http/Controllers/Admin/ScheduleEmployee/ScheduleEmployeeController.php
+++ app/Http/Controllers/Admin/ScheduleEmployee/ScheduleEmployeeController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Admin\ScheduleEmployee;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Admin\WorkScheduleEmployee\StoreRequest;
-use App\Http\Requests\Admin\WorkScheduleEmployee\UpdateRequest;
+use App\Http\Requests\Admin\EmployeeWorkSchedule\StoreRequest;
+use App\Http\Requests\Admin\EmployeeWorkSchedule\UpdateRequest;
 use App\Models\Admin;
 use App\Models\Booking;
 use App\Models\Shift;
