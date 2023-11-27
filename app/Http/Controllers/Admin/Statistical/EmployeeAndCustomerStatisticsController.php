c3c3cf8a077f451c7949592da426b0571d54aa3e
Thanh Tung <tunght2302@gmail.com>
2023-11-27 22:52:17 +0700

    format code,fix logic statis

--- app/Http/Controllers/Admin/Statistical/EmployeeAndCustomerStatisticsController.php
+++ app/Http/Controllers/Admin/Statistical/EmployeeAndCustomerStatisticsController.php
@@ -9,104 +9,105 @@ use Illuminate\Http\Request;
 
 class EmployeeAndCustomerStatisticsController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $topBooker = $this->basetopBooker();
         $topEmployeesData = $this->baseTopEmployees();
         return view('admin.Statistical.employeeAndCustomerStatistics', compact('topBooker', 'topEmployeesData'));
     }
 
     public function topBooker(Request $request)
-	{
-		$bookerData = $this->basefilterTopBooker($request);
-		return response()->json(['bookerData' => $bookerData]);
-	}
+    {
+        $bookerData = $this->basefilterTopBooker($request);
+        return response()->json(['bookerData' => $bookerData]);
+    }
 
     private function basetopBooker()
-	{
-		$query = Bill::select('bills.name', 'users.avatar')
-			->selectRaw('count(bills.id) as totalBookings')
-			->selectRaw('SUM(bills.total_price) as totalPrice')
-			->join('users', 'bills.user_id', '=', 'users.id')
-			->groupBy('bills.name', 'users.avatar')
-			->orderBy('totalBookings', 'desc')
-			->take(5)
-			->get();
-		return $query;
-	}
+    {
+        $query = Bill::select('bills.name', 'users.avatar')
+            ->selectRaw('count(bills.id) as totalBookings')
+            ->selectRaw('SUM(bills.total_price) as totalPrice')
+            ->join('users', 'bills.user_id', '=', 'users.id')
+            ->groupBy('bills.name', 'users.avatar')
+            ->orderBy('totalBookings', 'desc')
+            ->orderBy('totalPrice', 'desc')
+            ->take(5)
+            ->get();
+        return $query;
+    }
 
     private function basefilterTopBooker(Request $request)
-	{
-		$month = $request->month;
-		$year = $request->year;
+    {
+        $month = $request->month;
+        $year = $request->year;
 
-		$query = Bill::join('users', 'users.id', '=', 'bills.user_id')
-			->select('bills.name', 'users.avatar', 'bills.user_id')
-			->selectRaw('SUM(bills.total_price) as totalPrice')
-			->selectRaw('COUNT(*) as totalBookings')
-			->groupBy('bills.user_id', 'bills.name', 'users.avatar')
-			->orderByDesc('totalBookings');
-		if ($month) {
-			$query->whereMonth('day', $month);
-		}
+        $query = Bill::join('users', 'users.id', '=', 'bills.user_id')
+            ->select('bills.name', 'users.avatar', 'bills.user_id')
+            ->selectRaw('SUM(bills.total_price) as totalPrice')
+            ->selectRaw('COUNT(*) as totalBookings')
+            ->groupBy('bills.user_id', 'bills.name', 'users.avatar')
+            ->orderByDesc('totalBookings');
+        if ($month) {
+            $query->whereMonth('day', $month);
+        }
 
-		if ($year) {
-			$query->whereYear('day', $year);
-		}
+        if ($year) {
+            $query->whereYear('day', $year);
+        }
 
-		$bookerData = $query->take(5)->get();
-		return $bookerData;
-	}
+        $bookerData = $query->take(5)->get();
+        return $bookerData;
+    }
 
     private function baseTopEmployees()
-	{
-		$query = Booking::selectRaw('admins.username, admins.avatar, COUNT(DISTINCT bookings.id) as totalBookings')
-			->selectRaw('COUNT(DISTINCT reviews.id) as totalRatings')
-			->selectRaw('IFNULL(AVG(reviews.star), 0) as avgRating')
-			->join('admins', 'admins.id', '=', 'bookings.admin_id')
-			->leftJoin('reviews', function ($join) {
-				$join->on('admins.id', '=', 'reviews.admin_id');
-			})
-			->groupBy('admins.id', 'admins.username', 'admins.avatar'); // Include 'admins.username' and 'admins.avatar' in the GROUP BY
+    {
+        $query = Booking::selectRaw('admins.username, admins.avatar, COUNT(DISTINCT bookings.id) as totalBookings')
+            ->selectRaw('COUNT(DISTINCT reviews.id) as totalRatings')
+            ->selectRaw('IFNULL(AVG(reviews.star), 0) as avgRating')
+            ->join('admins', 'admins.id', '=', 'bookings.admin_id')
+            ->leftJoin('reviews', function ($join) {
+                $join->on('admins.id', '=', 'reviews.admin_id');
+            })
+            ->groupBy('admins.id', 'admins.username', 'admins.avatar');
 
-		$data = $query->orderByDesc('totalBookings')
-			->take(5)
-			->get();
+        $data = $query->orderByDesc('totalBookings')
+            ->take(5)
+            ->get();
 
-		return $data;
-	}
+        return $data;
+    }
 
     // Top 5 nhân viên nhiều lịch đặt
-	public function topEmployee(Request $request)
-	{
-		$topEmployeesData = $this->basefilterTopEmployee($request);
-		return response()->json(['topEmployeesData' => $topEmployeesData]);
-	}
+    public function topEmployee(Request $request)
+    {
+        $topEmployeesData = $this->basefilterTopEmployee($request);
+        return response()->json(['topEmployeesData' => $topEmployeesData]);
+    }
 
     private function basefilterTopEmployee(Request $request)
-	{
-		$month = $request->month;
-		$year = $request->year;
+    {
+        $month = $request->month;
+        $year = $request->year;
 
-		$query = Booking::selectRaw('admins.username, admins.avatar, COUNT(DISTINCT bookings.id) as totalBookings')
-			->selectRaw('COUNT(DISTINCT reviews.id) as totalRatings')
-			->selectRaw('IFNULL(AVG(reviews.star), 0) as avgRating')
-			->join('admins', 'admins.id', '=', 'bookings.admin_id')
-			->leftJoin('reviews', function ($join) {
-				$join->on('admins.id', '=', 'reviews.admin_id');
-			})
-			->groupBy('admins.id');
+        $query = Booking::selectRaw('admins.id, admins.username, admins.avatar')
+            ->selectRaw('COUNT(DISTINCT bookings.id) as totalBookings')
+            ->selectRaw('COUNT(DISTINCT reviews.id) as totalRatings')
+            ->selectRaw('IFNULL(AVG(reviews.star), 0) as avgRating')
+            ->join('admins', 'admins.id', '=', 'bookings.admin_id')
+            ->leftJoin('reviews', function ($join) {
+                $join->on('admins.id', '=', 'reviews.admin_id');
+            })
+            ->groupBy('admins.id', 'admins.username', 'admins.avatar');
 
-		if ($month) {
-			$query->whereMonth('bookings.day', $month);
-		}
+        if ($month) {
+            $query->whereMonth('bookings.day', $month);
+        }
 
-		if ($year) {
-			$query->whereYear('bookings.day', $year);
-		}
+        if ($year) {
+            $query->whereYear('bookings.day', $year);
+        }
 
-		$topEmployeesData = $query->orderByDesc('totalBookings')->take(5)->get();
-		return $topEmployeesData;
-	}
-
-
+        $topEmployeesData = $query->orderByDesc('totalBookings')->take(5)->get();
+        return $topEmployeesData;
+    }
 }
