08e924db7e3abc271abaa05df446e2672bec2c44
Thanh Tung <tunght2302@gmail.com>
2023-12-04 17:40:11 +0700

    fix logic statistical service

--- app/Http/Controllers/Admin/Statistical/ServiceUsageStatisticsController.php
+++ app/Http/Controllers/Admin/Statistical/ServiceUsageStatisticsController.php
@@ -8,76 +8,181 @@ use Illuminate\Support\Facades\DB;
 
 class ServiceUsageStatisticsController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $topservice = $this->baseServiceSetbyTime();
-        return view('admin.Statistical.serviceUsageStatistics',compact('topservice'));
+        return view('admin.Statistical.serviceUsageStatistics', compact('topservice'));
     }
 
     private function baseServiceSetbyTime()
-	{
-		// Tổng số lần sử dụng mỗi dịch vụ
-		$statistics =  DB::table('bill_details')
-			->join('services', 'bill_details.service_id', '=', 'services.id')
-			->select('bill_details.service_id', 'services.name', DB::raw('COUNT(*) as count'))
-			->groupBy('bill_details.service_id', 'services.name')
-			->having('count', '>=', 0) // Chỉ lấy những giá trị có count lớn hơn 1 (tức là trùng nhau)
-			->orderByDesc('count') // Sắp xếp theo count giảm dần
-			->get();
+    {
+        // Lấy năm hiện tại
+        $currentYear = now()->year;
 
-		// Định dạng dữ liệu để trả về
-		$topservice = [];
-		foreach ($statistics as $item) {
-			$serviceId = $item->service_id;
-			$name = $item->name;
+        // Tổng số lần sử dụng mỗi dịch vụ trong năm hiện tại và tổng doanh thu
+        $statistics = DB::table('bill_details')
+            ->join('bills', 'bills.id', '=', 'bill_details.bill_id')
+            ->join('services', 'bill_details.service_id', '=', 'services.id')
+            ->select(
+                'bill_details.service_id',
+                'services.name',
+                DB::raw('COUNT(*) as count'),
+                DB::raw('SUM(bills.total_price) as totalRevenues')
+            )
+            ->groupBy('bill_details.service_id', 'services.name')
+            ->having('count', '>=', 0)
+            ->whereYear('bills.day', $currentYear) // Lọc theo năm hiện tại
+            ->orderByDesc('count')
+            ->get();
 
-			$topservice[$serviceId] = [
-				'name' => $name,
-				'count' => $item->count,
-			];
-		}
+        // Tổng doanh thu của năm hiện tại
+        $totalRevenue = DB::table('bill_details')
+            ->join('bills', 'bills.id', '=', 'bill_details.bill_id')
+            ->whereYear('bills.day', $currentYear)
+            ->sum('bills.total_price');
 
-		return $topservice;
-	}
+        // Định dạng dữ liệu để trả về
+        $serviceStatistics = [];
+
+        foreach ($statistics as $item) {
+            $serviceId = $item->service_id;
+            $name = $item->name;
+            $count = $item->count;
+            $totalRevenues = $item->totalRevenues;
+
+            // Tính phần trăm chiếm trong tổng doanh thu
+            $percentage = ($totalRevenues / $totalRevenue) * 100;
+
+            $serviceStatistics[$serviceId] = [
+                'name' => $name,
+                'count' => $count,
+                'totalRevenues' => $totalRevenues,
+                'percentage' => $percentage,
+            ];
+        }
+
+        // Sắp xếp theo phần trăm giảm dần và lấy ra 5 dịch vụ
+        $sortedServiceStatistics = collect($serviceStatistics)->sortByDesc('percentage')->take(5)->toArray();
+
+        // Tính tổng phần trăm của 5 dịch vụ đầu tiên
+        $totalPercentage = array_sum(array_column($sortedServiceStatistics, 'percentage'));
+
+        // Tính tổng count và totalRevenues của tất cả các dịch vụ
+        $totalCount = DB::table('bill_details')
+            ->join('bills', 'bills.id', '=', 'bill_details.bill_id')
+            ->whereYear('bills.day', $currentYear)
+            ->count();
+
+        $totalRevenuesAll = DB::table('bill_details')
+            ->join('bills', 'bills.id', '=', 'bill_details.bill_id')
+            ->whereYear('bills.day', $currentYear)
+            ->sum('bills.total_price');
+
+        // Lấy count và totalRevenues của phần tử "Others"
+        $othersCount = $totalCount - array_sum(array_column($sortedServiceStatistics, 'count'));
+        $othersTotalRevenues = $totalRevenuesAll - array_sum(array_column($sortedServiceStatistics, 'totalRevenues'));
+
+        // Tạo phần tử "Others" với count và totalRevenues
+        $others = [
+            'name' => 'Dịch vụ khác',
+            'count' => $othersCount,
+            'totalRevenues' => $othersTotalRevenues,
+            'percentage' => 100 - $totalPercentage,
+        ];
+
+        // Thêm phần tử "Others" vào mảng kết quả
+        $result = $sortedServiceStatistics;
+        $result[] = $others;
+
+        return $result;
+    }
+
     public function ServiceSetbyTime(Request $request)
-	{
-		$topservice = $this->baServiceSetbyTime($request);
-		return response()->json(['topservice' => $topservice]);
-	}
+    {
+        $topservice = $this->baServiceSetbyTime($request);
+        return response()->json(['topservice' => $topservice]);
+    }
 
-	private function baServiceSetbyTime(Request $request)
-	{
-		$month = $request->month;
-		$year = $request->year;
-        
-		$statistics = DB::table('bill_details')
-			->join('services', 'bill_details.service_id', '=', 'services.id')
-			->join('bills', 'bill_details.bill_id', '=', 'bills.id') // Join với bảng bill
-			->select('bill_details.service_id', 'services.name', DB::raw('COUNT(*) as count'))
-			->groupBy('bill_details.service_id', 'services.name')
-			->having('count', '>=', 0)
-			->when($month, function ($query, $month) {
-				return $query->whereMonth('bills.day', $month); // Sử dụng cột day từ bảng bill
-			})
-			->when($year, function ($query, $year) {
-				return $query->whereYear('bills.day', $year); // Sử dụng cột day từ bảng bill
-			})
-			->orderByDesc('count')
-			->get();
-		$topservice = [];
-		foreach ($statistics as $item) {
-			$serviceId = $item->service_id;
-			$name = $item->name;
-			$topservice[$serviceId] = [
-				'name' => $name,
-				'count' => $item->count,
-			];
-		}
-		if (empty($topservice)) {
-			$topservice[] = [
-				'name' => "Không có dữ liệu",
-				'count' => 0,
-			];
-		}
-		return $topservice;
-	}
+    private function baServiceSetbyTime(Request $request)
+    {
+        // Lấy năm từ request hoặc năm hiện tại nếu không có
+        $year = $request->year ?? now()->year;
+
+        // Tổng số lần sử dụng mỗi dịch vụ trong năm và tổng doanh thu
+        $statistics = DB::table('bill_details')
+            ->join('bills', 'bills.id', '=', 'bill_details.bill_id')
+            ->join('services', 'bill_details.service_id', '=', 'services.id')
+            ->select(
+                'bill_details.service_id',
+                'services.name',
+                DB::raw('COUNT(*) as count'),
+                DB::raw('SUM(bills.total_price) as totalRevenues')
+            )
+            ->groupBy('bill_details.service_id', 'services.name')
+            ->having('count', '>=', 0)
+            ->whereYear('bills.day', '=', $year) // Sửa thành "="
+            ->orderByDesc('count')
+            ->get();
+
+        // Tổng doanh thu của năm
+        $totalRevenue = DB::table('bill_details')
+            ->join('bills', 'bills.id', '=', 'bill_details.bill_id')
+            ->whereYear('bills.day', '=', $year) // Sửa thành "="
+            ->sum('bills.total_price');
+
+        // Định dạng dữ liệu để trả về
+        $serviceStatistics = [];
+
+        foreach ($statistics as $item) {
+            $serviceId = $item->service_id;
+            $name = $item->name;
+            $count = $item->count;
+            $totalRevenues = $item->totalRevenues;
+
+            // Tính phần trăm chiếm trong tổng doanh thu
+            $percentage = ($totalRevenues / $totalRevenue) * 100;
+
+            $serviceStatistics[$serviceId] = [
+                'name' => $name,
+                'count' => $count,
+                'totalRevenues' => $totalRevenues,
+                'percentage' => $percentage,
+            ];
+        }
+
+        // Sắp xếp theo phần trăm giảm dần và lấy ra 5 dịch vụ
+        $sortedServiceStatistics = collect($serviceStatistics)->sortByDesc('percentage')->take(5)->toArray();
+
+        // Tính tổng phần trăm của 5 dịch vụ đầu tiên
+        $totalPercentage = array_sum(array_column($sortedServiceStatistics, 'percentage'));
+
+        // Tính tổng count và totalRevenues của tất cả các dịch vụ
+        $totalCount = DB::table('bill_details')
+            ->join('bills', 'bills.id', '=', 'bill_details.bill_id')
+            ->whereYear('bills.day', '=', $year) // Sửa thành "="
+            ->count();
+
+        $totalRevenuesAll = DB::table('bill_details')
+            ->join('bills', 'bills.id', '=', 'bill_details.bill_id')
+            ->whereYear('bills.day', '=', $year) // Sửa thành "="
+            ->sum('bills.total_price');
+
+        // Lấy count và totalRevenues của phần tử "Others"
+        $othersCount = $totalCount - array_sum(array_column($sortedServiceStatistics, 'count'));
+        $othersTotalRevenues = $totalRevenuesAll - array_sum(array_column($sortedServiceStatistics, 'totalRevenues'));
+
+        // Tạo phần tử "Others" với count và totalRevenues
+        $others = [
+            'name' => 'Dịch vụ khác',
+            'count' => $othersCount,
+            'totalRevenues' => $othersTotalRevenues,
+            'percentage' => 100 - $totalPercentage,
+        ];
+
+        // Thêm phần tử "Others" vào mảng kết quả
+        $result = $sortedServiceStatistics;
+        $result[] = $others;
+
+        return $result;
+    }
 }
