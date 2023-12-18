551d46f8118a4d4d7ce6ff43c37f340f40f1301d
Duonglor <quyduong1201@gmail.com>
2023-12-18 15:57:31 +0700

    fix name js fix nav client

--- resources/views/admin/Statistical/serviceUsageStatistics.blade.php
+++ resources/views/admin/Statistical/serviceUsageStatistics.blade.php
@@ -7,7 +7,7 @@
                 <div class="col-span-12 sm:col-span-6 lg:col-span-6 mt-8">
                     <div class="intro-y h-10 mb-5 flex flex-col lg:flex-row justify-between items-center" style="display: flex;justify-content: space-between">
                         <h2 class="text-lg font-medium truncate mr-5">
-                            Thống kê dịch vụ
+                            Thống kê doanh thu dịch vụ theo năm
                         </h2>
                         <form id="filterTopService" method="POST">
                             @csrf
